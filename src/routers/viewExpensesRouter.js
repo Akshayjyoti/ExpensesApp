980dafcffa850f8ca83f195826a1fa00be9e6c19
Akshayjyoti <ajb.akshayjbordoloi@gmail.com>
2023-02-15 16:25:10 +0530

    Changed from MongoClient to Mongoose for viewExpensesRouter

--- src/routers/viewExpensesRouter.js
+++ src/routers/viewExpensesRouter.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import debug from 'debug';
-import chalk from 'chalk';
-import { MongoClient, ObjectId } from 'mongodb';
+import expenseModel from '../data/models/expenseModel.js';
 
 const viewExpensesRouter = express.Router();
 const myDebug = debug('app:viewExpensesRouter');
@@ -15,26 +14,15 @@ viewExpensesRouter.use((req, res, next) => {
 });
 
 viewExpensesRouter.route('/').get((req, res) => {
-    const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2';
-    const dbName = 'expensesApp';
 
-    (async function mongo() {
-        let client;
-        try {
-            client = await MongoClient.connect(url);
-            myDebug(chalk.green('Connected correctly to the server'));
-
-            const db = client.db(dbName);
-
-            const expenses = await db.collection('expenses').find().toArray();
+    expenseModel.find( {}, (err, expenses) => {
+        if(err) {
+            console.log(err);
+        } else {
+            // console.log(data);
             res.render('csvUpload', { expenses });
-
-        } catch(err) {
-            myDebug(err.stack);
         }
-
-        client.close();
-    }());
+    });
 });
 
 export default viewExpensesRouter;
