6030579dd95309393c2fec71209ac0bdcfd00385
Akshayjyoti <ajb.akshayjbordoloi@gmail.com>
2023-02-15 17:01:53 +0530

    Changed from MongoClient to Mongoose for editExpenseRouter

--- src/routers/editExpenseRouter.js
+++ src/routers/editExpenseRouter.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import debug from 'debug';
-import chalk from 'chalk';
-import { MongoClient, ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import currencyConversion from '../data/currencyConversion.js';
 import dateConversion from '../data/dateConversion.js';
+import expenseModel from '../data/models/expenseModel.js';
 
 const editExpenseRouter = express.Router();
 const myDebug = debug('app:editExpenseRouter');
@@ -17,50 +17,26 @@ editExpenseRouter.use((req, res, next) => {
 });
 
 editExpenseRouter.route('/select').get((req, res) => {
-    const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2';
-    const dbName = 'expensesApp';
 
-    (async function mongo() {
-        let client;
-        try {
-            client = await MongoClient.connect(url);
-            myDebug(chalk.green('Connected correctly to the server'));
-
-            const db = client.db(dbName);
-
-            const expenses = await db.collection('expenses').find().toArray();
+    expenseModel.find( {}, (err, expenses) => {
+        if(err) {
+            console.log(err);
+        } else {
             res.render('selectEditExpense', { expenses });
-
-        } catch(err) {
-            myDebug(err.stack);
         }
-
-        client.close();
-    }());
+    });
 });
 
 editExpenseRouter.route('/edit').post((req, res) => {
     const id = Object.keys(req.body)[0];
-    const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2';
-    const dbName = 'expensesApp';
 
-    (async function mongo() {
-        let client;
-        try {
-            client = await MongoClient.connect(url);
-            myDebug(chalk.green('Connected correctly to the server'));
-
-            const db = client.db(dbName);
-
-            const expense = await db.collection('expenses').findOne({ _id: new ObjectId(id)});
+    expenseModel.findOne( { _id: new ObjectId(id) }, (err, expense) => {
+        if(err) {
+            console.log(err);
+        } else {
             res.render('editExpense', { expense });
-
-        } catch(err) {
-            myDebug(err.stack);
         }
-
-        client.close();
-    }());
+    });
 });
 
 
@@ -77,27 +53,13 @@ editExpenseRouter.route('/editExecute').post((req, res) => {
         Amount_INR: Math.round(newValues[2] * currencyConversion(newValues[3]) * 100) / 100
     }
 
-    const url = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2';
-    const dbName = 'expensesApp';
-
-    (async function mongo() {
-        let client;
-        try {
-            client = await MongoClient.connect(url);
-            const db = client.db(dbName);
-            
-            const results = await db.collection('expenses').updateOne({ _id : new ObjectId(id)}, { $set: newExpenseJSON});
-            
-            console.log(results);
-            
+    expenseModel.findOneAndUpdate( { _id: new ObjectId(id) }, newExpenseJSON, (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
             res.redirect('/editExpense/select');
-
-        } catch(err) {
-            myDebug(err.stack);
         }
-
-        client.close();
-    }());
+    });
 });
 
 export default editExpenseRouter;
